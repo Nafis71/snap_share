e338c1d2dc62860e2abac3e0e23013f2a1dad108
Nafis71 <nhtonmoy2@gmail.com>
2024-11-26 16:57:45 +0600

    file structure updated

---
+++ lib/app/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SnapShare extends StatelessWidget {
+  const SnapShare({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
