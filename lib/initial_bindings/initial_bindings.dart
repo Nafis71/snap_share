bf9fd1283d4fc405eb6cdef116e45eb3c6dc71b1
dev-asib <mail.asib.com@gmail.com>
2024-11-27 22:13:26 +0600

    Changed App name & Changed DependencyInjection to Dependency

--- lib/initial_bindings/initial_bindings.dart
+++ lib/initial_bindings/initial_bindings.dart
@@ -1,7 +1,7 @@
 import 'package:get/get.dart';
 import 'package:snap_share/export_file.dart';
 
-class DependencyInjection {
+class Dependency{
   static void init() {
     Get.lazyPut(() => SplashViewModel(), fenix: true);
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,6 @@ import 'package:snap_share/app.dart';
 import 'package:snap_share/export_file.dart';
 
 main() {
-  DependencyInjection.init();
-  runApp(const MyApp());
+  Dependency.init();
+  runApp(const SnapShare());
 }
