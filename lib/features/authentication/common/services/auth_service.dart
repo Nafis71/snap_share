d49c25e0ec7bc59a09ffedd36c6a0023cbf0229c
Nafis71 <nhtonmoy2@gmail.com>
2024-12-03 17:26:45 +0600

    SSBT-3: Bug Fixes - removed trim

--- lib/features/authentication/common/services/auth_service.dart
+++ lib/features/authentication/common/services/auth_service.dart
@@ -8,14 +8,14 @@ class AuthService{
   Future<UserCredential> signUp(String email,String password)async{
     return await _firebaseAuth.createUserWithEmailAndPassword(
       email: email.trim(),
-      password: password.trim(),
+      password: password,
     );
   }
 
   Future<UserCredential> signIn(String email,String password)async{
     return await _firebaseAuth.signInWithEmailAndPassword(
       email: email.trim(),
-      password: password.trim(),
+      password: password,
     );
   }
 }
